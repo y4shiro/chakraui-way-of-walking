3959cd003cf625606f5e1f94450fba6346cc50f2
y4shiro <y4shironao@gmail.com>
2021-09-28 21:30:33 +0900

    Box コンポーネントのスタイルの付け方

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,23 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 
+import { Box } from '@chakra-ui/react';
+
 const Home: NextPage = () => {
-  return <h1>Chakra やっていき</h1>;
+  return (
+    <>
+      <h1>Chakra やっていき</h1>
+      <Box padding={10} backgroundColor="skyblue">
+        padding={10} / backgroundColor= skyblue
+      </Box>
+      <Box p={10} backgroundColor="lightgreen">
+        padding のショートハンド p={10}
+      </Box>
+      <Box px={10} my={10} backgroundColor="skyblue">
+        px=10 my=10
+      </Box>
+    </>
+  );
 };
 
 export default Home;
