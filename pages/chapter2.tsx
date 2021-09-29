7cf7ea602f562a50251b13245e1fba3f90a5d75f
y4shiro <y4shironao@gmail.com>
2021-09-29 16:54:48 +0900

    boxShadow や rounded などの Style Props

--- pages/chapter2.tsx
+++ pages/chapter2.tsx
@@ -1,11 +1,26 @@
 import type { NextPage } from 'next';
 
-import { Heading } from '@chakra-ui/react';
+import { Box, Heading, Button } from '@chakra-ui/react';
 
 const Chapter2: NextPage = () => {
   return (
     <>
       <Heading>Chapter2</Heading>
+      <Box m={4} p={4} boxShadow="md" backgroundColor="blue.100">
+        boxShadow=md を付与した
+      </Box>
+      <Box
+        m={4}
+        p={4}
+        rounded="full"
+        boxShadow="xl"
+        backgroundColor="lightgray"
+      >
+        rounded=full を付与した
+      </Box>
+      <Button m={4} shadow="md">
+        Button Shadow
+      </Button>
     </>
   );
 };
