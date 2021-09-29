50de9b4fb0edb9ec57410e30fbbbc94013a7c74a
y4shiro <y4shironao@gmail.com>
2021-09-29 17:23:50 +0900

    リストレイアウトの比較

--- pages/chapter2.tsx
+++ pages/chapter2.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
+import { Box, Button, Flex, Grid, Heading, Stack } from '@chakra-ui/react';
 
-import { Box, Heading, Button } from '@chakra-ui/react';
+const sampleText: string[] = ['dog', 'cat', 'rabbit'];
 
 const Chapter2: NextPage = () => {
   return (
@@ -21,6 +22,42 @@ const Chapter2: NextPage = () => {
       <Button m={4} shadow="md">
         Button Shadow
       </Button>
+
+      <h2>Box の場合</h2>
+      <Box>
+        {sampleText.map((item) => (
+          <Box p={2} bg="red.100" key={item}>
+            {item}
+          </Box>
+        ))}
+      </Box>
+
+      <h2>Stack の場合</h2>
+      <Stack>
+        {sampleText.map((item) => (
+          <Box p={2} bg="blue.100" key={item}>
+            {item}
+          </Box>
+        ))}
+      </Stack>
+
+      <h2>Grid の場合</h2>
+      <Grid gap={2}>
+        {sampleText.map((item) => (
+          <Box p={2} bg="green.100" key={item}>
+            {item}
+          </Box>
+        ))}
+      </Grid>
+
+      <h2>Flex の場合</h2>
+      <Flex direction="column" gridGap={2}>
+        {sampleText.map((item) => (
+          <Box p={2} bg="yellow.200" key={item}>
+            {item}
+          </Box>
+        ))}
+      </Flex>
     </>
   );
 };
