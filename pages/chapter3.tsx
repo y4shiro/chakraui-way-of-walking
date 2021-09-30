e4f38d76937ac1cfeb00d4bd23c9ae6cef2f2c70
y4shiro <y4shironao@gmail.com>
2021-09-30 11:56:03 +0900

    Drawer とレスポンシブを使ってモバイル / PC で表示が変わるメニューを作成

--- pages/chapter3.tsx
+++ pages/chapter3.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactNode, useRef, useState } from 'react';
 import type { NextPage } from 'next';
+import { useIntersection } from 'use-intersection';
 
 import {
   Accordion,
@@ -8,12 +9,23 @@ import {
   AccordionItem,
   AccordionPanel,
   Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
   Heading,
   HStack,
+  Link,
+  LinkProps,
   Skeleton,
   Spinner,
+  Stack,
+  useDisclosure,
 } from '@chakra-ui/react';
-import { useIntersection } from 'use-intersection';
+import { HamburgerIcon } from '@chakra-ui/icons';
 
 const animals = ['dog', 'cat', 'rabbit', 'mouse', 'bird'];
 
@@ -24,6 +36,14 @@ const Chapter3: NextPage = () => {
       <SomeItems items={animals} />
 
       <LazyLoadingAccordion title="open component">Hello</LazyLoadingAccordion>
+
+      <LayoutWithMenu>
+        <Box>
+          Humburger Humburger Humburger Humburger Humburger Humburger Humburger
+          Humburger Humburger Humburger Humburger Humburger Humburger Humburger
+          Humburger Humburger Humburger Humburger
+        </Box>
+      </LayoutWithMenu>
     </>
   );
 };
@@ -77,6 +97,69 @@ const LazyLoadingAccordion: FC<{ title: ReactNode }> = ({
         }}
       </AccordionItem>
     </Accordion>
+  );
+};
+
+const HoverLink = (props: LinkProps) => (
+  <Link rounded="base" _hover={{ bg: 'gray.200' }} p={2} {...props} />
+);
+
+const Navigation = () => {
+  return (
+    <Stack as="nav">
+      <HoverLink href="/bugrer">Burger</HoverLink>
+      <HoverLink href="/sidemenu">Sidemenu</HoverLink>
+      <HoverLink href="/drink">Drink</HoverLink>
+      <HoverLink href="/takeout">Takeout</HoverLink>
+    </Stack>
+  );
+};
+
+const DrawerMenu = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef<HTMLButtonElement>(null);
+
+  return (
+    <>
+      <Button ref={btnRef} onClick={onOpen}>
+        <HamburgerIcon />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        onClose={onClose}
+        placement="left"
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Menu</DrawerHeader>
+            <DrawerBody>
+              <Navigation />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
+  );
+};
+
+const LayoutWithMenu: FC<{}> = ({ children }) => {
+  return (
+    <Stack>
+      <HStack p={5}>
+        <Box display={{ base: 'block', md: 'none' }}>
+          <DrawerMenu />
+        </Box>
+        <Heading>Humberger Shop</Heading>
+      </HStack>
+      <HStack alignItems="start">
+        <Box display={{ base: 'none', md: 'block' }} w={500} px={6}>
+          <Navigation />
+        </Box>
+        <Box>{children}</Box>
+      </HStack>
+    </Stack>
   );
 };
 
