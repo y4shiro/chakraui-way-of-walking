0f2e96abd31ab9a44eb4e1b46de1402512b6130a
y4shiro <y4shironao@gmail.com>
2021-09-30 10:45:52 +0900

    Skeleton と Intersection Observer

--- pages/chapter3.tsx
+++ pages/chapter3.tsx
@@ -1,10 +1,40 @@
+import React, { FC, useRef } from 'react';
 import type { NextPage } from 'next';
-import { Heading } from '@chakra-ui/react';
+
+import { Box, Heading, Skeleton } from '@chakra-ui/react';
+import { useIntersection } from 'use-intersection';
+
+const animals = ['dog', 'cat', 'rabbit', 'mouse', 'bird'];
 
 const Chapter3: NextPage = () => {
   return (
     <>
       <Heading>Chapter3</Heading>
+      <SomeItems items={animals} />
+    </>
+  );
+};
+
+const LazyElement: FC<{}> = ({ children }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const intersecting = useIntersection(ref, {
+    rootMargin: '250px',
+    once: true,
+  });
+
+  return (
+    <div ref={ref}>
+      <Skeleton isLoaded={intersecting}>{children}</Skeleton>
+    </div>
+  );
+};
+
+const SomeItems = ({ items }) => {
+  return (
+    <>
+      {items.map((item: string) => {
+        return <LazyElement key={item}>{item}</LazyElement>;
+      })}
     </>
   );
 };
